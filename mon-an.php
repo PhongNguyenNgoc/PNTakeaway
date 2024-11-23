f71bafc0cc5c4aaa001e2fc54e82b99887e283c5
PhongNguyenNgoc <Nngocphong2503@gmail.com>
2024-11-23 15:27:38 +0700

    xem mon/tim kiem mon

--- mon-an.php
+++ mon-an.php
@@ -6,8 +6,8 @@ include('./callbackU/menuU.php');
 <section class="food-search text-center">
     <div class="container">
 
-        <form action="<?php echo SITEURL ?>food-search.php" method="POST">
-            <input type="search" name="search" placeholder="Tìm kiếm món ăn cụ thể" required>
+        <form action="<?php echo SITEURL ?>tim-kiem.php" method="POST">
+            <input type="search" name="timKiem" placeholder="Tìm kiếm món ăn cụ thể" required>
             <button type="submit" class="btn btn-primary">Tìm kiếm</button>
         </form>
 
@@ -17,112 +17,39 @@ include('./callbackU/menuU.php');
 <div class="container py5">
     <h3 class="text-center">Món ăn tại PN Takeaway</h3>
     <div class="row row-cols-1 row-cols-md-4 g-4 py-5">
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100 000d</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
 
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
+        <?php
+        $sql = "SELECT * FROM mon_an WHERE TRANGTHAI=1";
+        $res = mysqli_query($conn, $sql);
+        $count = mysqli_num_rows($res);
+        if ($count > 0) {
+            while ($row = mysqli_fetch_assoc($res)) {
+                $idF = $row['ID_MONAN'];
+                $idT = $row['ID_LOAIMONAN'];
+                $nF = $row['TENMONAN'];
+                $p = $row['GIATIEN'];
+                $detailF = $row['CHITIETMONAN'];
+                $img = $row['ANH'];
 
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
+        ?> <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?php echo SITEURL . "img/food/" . $img; ?>" class="card-img-top" alt="..." width="250" height="250">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $nF; ?></h5>
+                            <p class="card-text"><?php echo $detailF; ?></p>
+                        </div>
+                        <div class="d-flex justify-content-around mb-5">
+                            <h5><?php echo $p; ?> đ</h5>
+                            <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
+                        </div>
+                    </div>
+                </div><?php
 
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
-
+                    }
+                } ?>
 
     </div>
 </div>
-
-
-
-
-
 
 <!--Chan trang web-->
 <?php
--- tim-kiem.php
+++ tim-kiem.php
@@ -6,7 +6,7 @@ include('./callbackU/menuU.php');
 <section class="food-search text-center">
     <div class="container">
 
-        <h2>Kết quả cho "<a href="#" class="text-white">aaaaaaaaa</a>"</h2>
+        <h2>Kết quả cho "<a href="#" class="text-white"><?php echo $_POST['timKiem']; ?></a>"</h2>
 
     </div>
 </section>
@@ -14,105 +14,42 @@ include('./callbackU/menuU.php');
 <div class="container py5">
 
     <div class="row row-cols-1 row-cols-md-4 g-4 py-5">
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100 000d</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
 
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
+        <?php
+        $txt = $_POST['timKiem'];
+        $sql = "SELECT MA.* FROM MON_AN MA JOIN LOAI_MON_AN LMA ON MA.ID_LOAIMONAN = LMA.ID_LOAIMONAN WHERE LMA.TENLOAI LIKE ('%$txt%') OR MA.TENMONAN LIKE ('%$txt%')";
+        $res = mysqli_query($conn, $sql);
+        $count = mysqli_num_rows($res);
 
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
+        if ($res) {
+            if ($count > 0) {
+                while ($row = mysqli_fetch_assoc($res)) {
+                    $idF = $row['ID_MONAN'];
+                    $idT = $row['ID_LOAIMONAN'];
+                    $nF = $row['TENMONAN'];
+                    $p = $row['GIATIEN'];
+                    $detailF = $row['CHITIETMONAN'];
+                    $img = $row['ANH'];
 
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
-
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-                <img src="./img/user/ex.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Tên món ăn</h5>
-                    <p class="card-text">Chi tiết món ăn</p>
-                </div>
-                <div class="d-flex justify-content-around mb-5">
-                    <h5>100</h5>
-                    <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
-                </div>
-            </div>
-        </div>
-
-
+        ?> <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="<?php echo SITEURL . "img/food/" . $img; ?>" class="card-img-top" alt="..." width="250" height="250">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $nF; ?></h5>
+                                <p class="card-text"><?php echo $detailF; ?></p>
+                            </div>
+                            <div class="d-flex justify-content-around mb-5">
+                                <h5><?php echo $p; ?> đ</h5>
+                                <button class="btn btn-primary"><a href="#">Thêm vào giỏ hàng</a></button>
+                            </div>
+                        </div>
+                    </div><?php
+                        }
+                    } else {
+                            ?><h3 class="text-center" style="color: red;">Không có dữ liệu</h3><?php
+                                                                                            }
+                                                                                        }
+                                                                                                ?>
     </div>
 </div>
 
